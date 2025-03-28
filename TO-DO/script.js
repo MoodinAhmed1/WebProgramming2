76d909bcdf7bcd4e62352b62610d98437dce69fe
Your Name <your.email@example.com>
2025-03-28 01:43:46 -0700

    class 102 (kinda not complete)

--- TO-DO/script.js
+++ TO-DO/script.js
@@ -6,22 +6,50 @@ function addToDo () {
     if(userInput == "") {
         alert('input cannot be empty');
         return;
-    } else {    
-        List.push(userInput);
-        localStorage.setItem('list', JSON.stringify(List));
+    }    
 
-        renderList();
-        document.querySelector('.input').value = "";
-    }
+    List.push(userInput);
+    localStorage.setItem('list', JSON.stringify(List));
+
+    renderList();
+    document.querySelector('.input').value = "";
 }
+
 function renderList() {
     const toDoList = document.querySelector('.toDoList');
 
-    toDoList.innerHTML = "";
+    toDoList.innerHTML = `
+                <tr>
+                    <th>Tasks</th>
+                    <th>Action</th>
+                </tr>
+            `;
 
-        List.forEach(toDO => {
-            toDoList.innerHTML += `<li>${toDO}</li>`;
-        })
+        List.forEach((toDO, index) => {
+            const row = document.createElement('tr');
+
+            //create task cell;
+            const taskCell = document.createElement('td');
+            taskCell.textContent = toDO;
+            row.appendChild(taskCell);
+
+            //create action cell;
+            const actionCell = document.createElement('td');
+            const deleteBtn = document.createElement('button');
+            deleteBtn.textContent = "delete";
+            deleteBtn.addEventListener('click',() => removeToDo(index));
+            actionCell.appendChild(deleteBtn);
+            row.appendChild(actionCell);
+
+            //Append the row to the table
+            toDoList.appendChild(row);
+        });
+}
+
+function removeToDo(index) {
+    List.splice(index, 1);
+    localStorage.setItem('list', JSON.stringify(List));
+    renderList();
 }
 
 document.addEventListener('DOMContentLoaded', renderList)
